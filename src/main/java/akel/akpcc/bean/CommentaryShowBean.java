0978dc424f386b96f0f4f0f486a13038fa5b52cd
fussy113 <s1220080@gmail.com>
2018-01-25 18:33:33 +0900

    showbeanの用意

--- src/main/java/akel/akpcc/bean/CommentaryShowBean.java
+++ src/main/java/akel/akpcc/bean/CommentaryShowBean.java
@@ -7,4 +7,26 @@ import java.io.Serializable;
 @Named
 @ViewScoped
 public class CommentaryShowBean implements Serializable{
+    private int id;
+    private String who;
+
+    public void show(){
+
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public String getWho(){
+        return who;
+    }
+
+    public void setWho(String who){
+        this.who = who;
+    }
 }
